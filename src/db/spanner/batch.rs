mozilla-services/syncstorage-rs

commit 8d3264f7f207034131250ddb93f013a64ab38d05

fix: lower max_total_records per batch_commit_update costs
and remove the potential, redundant touch_collection on batch commit

recalculating the mutations (see adca8d67):

- 4 for touch_collection: always an UPDATE on batch commit (due to
  pretouch_collection). we thought this cost 1 but it's probably 4 (see
  #333)
- 19992 (1666 max_total_records * 12, for the UPDATE case, see #333) for
  the mix of post_bsos and batch_commit
- 2 for batch delete (1 for the delete and 1 BsoExpiry update)

total: 19998

Closes #333

src/db/spanner/models.rs
@@ -81,6 +81,8 @@ pub(super) struct SpannerDbSession {
     pub(super) mutations: Option<Vec<Mutation>>,
     in_write_transaction: bool,
     execute_sql_count: u64,
+    /// Whether touch_collection has already been called
+    touched_collection: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -816,6 +818,11 @@ impl SpannerDb {
         // buffered on the client side and only issued to Spanner in the final
         // transaction Commit.
         let timestamp = self.timestamp()?;
+        if self.session.borrow().touched_collection {
+            // No need to touch it again
+            return Ok(timestamp);
+        }
+
         let sqlparams = params! {
             "fxa_uid" => user_id.fxa_uid.clone(),
             "fxa_kid" => user_id.fxa_kid.clone(),
@@ -858,6 +865,7 @@ impl SpannerDb {
             .param_types(sql_types)
             .execute(&self.conn)?;
         }
+        self.session.borrow_mut().touched_collection = true;
         Ok(timestamp)
     }
 

spanner_config.ini
@@ -1,2 +1,2 @@
 # Temp limit on the number of incoming records (See issue #298/#333)
-limits.max_total_records=1999
+limits.max_total_records=1666

src/web/handlers.rs
@@ -259,10 +259,6 @@ pub fn post_collection_batch(
                 // Spanner we would pay twice the mutations for those pending
                 // items (once writing them to to batch_bsos, then again
                 // writing them to bsos)
-
-                // NOTE: Unfortunately this means we make two calls to
-                // touch_collection (in post_bsos and then commit_batch). The
-                // second touch is redundant, writing the same timestamp
                 Either::A(
                     coll.db
                         .post_bsos(params::PostBsos {

commit 0e539d50d4a1154f5cc880faf5daa2482a1373fe

feat: route reads through the streaming sql api
writes continue to use plain execute sql (via execute_dml)

and fix db-tests under spanner (which I broke in 8d3264f7 w/ the
SpannerDbSession::touched_collection flag)

Closes #205

Cargo.toml
@@ -37,6 +37,7 @@ lazy_static = "1.4.0"
 hawk = "3.0.0"
 hkdf = "0.8.0"
 hmac = "0.7"
+itertools = "0.8.2"
 log = { version = "0.4.8", features = ["max_level_info", "release_max_level_info"] }
 mime = "0.3"
 mozsvc-common = "0.1"

src/db/spanner/batch.rs
@@ -40,7 +40,7 @@ pub fn create(db: &SpannerDb, params: params::CreateBatch) -> Result<results::Cr
     .param_types(param_types! {
         "expiry" => TypeCode::TIMESTAMP,
     })
-    .execute(&db.conn)?;
+    .execute_dml(&db.conn)?;
 
     do_append(
         db,
@@ -148,7 +148,7 @@ pub fn delete(db: &SpannerDb, params: params::DeleteBatch) -> Result<()> {
         "collection_id" => collection_id.to_string(),
         "batch_id" => params.id,
     })
-    .execute(&db.conn)?;
+    .execute_dml(&db.conn)?;
     Ok(())
 }
 
@@ -277,7 +277,7 @@ pub fn commit(db: &SpannerDb, params: params::CommitBatch) -> Result<results::Co
         .param_types(param_types! {
             "timestamp" => TypeCode::TIMESTAMP,
         })
-        .execute(&db.conn);
+        .execute_dml(&db.conn);
     _audit_commit_err(db, &params, result)?;
 
     // Then INSERT INTO SELECT remaining rows from this batch into the bsos
@@ -296,7 +296,7 @@ pub fn commit(db: &SpannerDb, params: params::CommitBatch) -> Result<results::Co
             "timestamp" => TypeCode::TIMESTAMP,
             "default_bso_ttl" => TypeCode::INT64,
         })
-        .execute(&db.conn);
+        .execute_dml(&db.conn);
     _audit_commit_err(db, &params, result)?;
 
     delete(
@@ -397,7 +397,7 @@ pub fn do_append(
     )?
     .params(sqlparams)
     .param_types(sqlparam_types)
-    .execute(&db.conn)?;
+    .execute_dml(&db.conn)?;
 
     Ok(())
 }
@@ -437,7 +437,7 @@ fn pretouch_collection(db: &SpannerDb, user_id: &HawkIdentifier, collection_id: 
         .param_types(param_types! {
             "modified" => TypeCode::TIMESTAMP,
         })
-        .execute(&db.conn)?;
+        .execute_dml(&db.conn)?;
     }
     Ok(())
 }

src/db/spanner/models.rs
@@ -16,12 +16,11 @@ use super::pool::CollectionCache;
 use crate::db::{
     error::{DbError, DbErrorKind},
     params, results,
-    spanner::support::{as_type, SyncResultSet},
+    spanner::support::{as_type, MapAndThenTrait, StreamedResultSet},
     util::SyncTimestamp,
     Db, DbFuture, Sorting, FIRST_CUSTOM_COLLECTION_ID,
 };
 use crate::server::metrics::Metrics;
-
 use crate::web::extractors::{BsoQueryParams, HawkIdentifier};
 
 #[cfg(not(any(test, feature = "db_test")))]
@@ -40,6 +39,7 @@ use googleapis_raw::spanner::v1::{
     spanner::{BeginTransactionRequest, CommitRequest, ExecuteSqlRequest, RollbackRequest},
     type_pb::TypeCode,
 };
+use itertools::Itertools;
 
 #[allow(unused_imports)]
 use protobuf::{well_known_types::ListValue, Message, RepeatedField};
@@ -171,7 +171,7 @@ impl SpannerDb {
         // This should always run within a r/w transaction, so that: "If a
         // transaction successfully commits, then no other writer modified the
         // data that was read in the transaction after it was read."
-        if cfg!(not(any(test, feature = "db_test"))) && !self.in_write_transaction() {
+        if !cfg!(any(test, feature = "db_test")) && !self.in_write_transaction() {
             Err(DbError::internal("Can't escalate read-lock to write-lock"))?
         }
         let result = self
@@ -195,7 +195,7 @@ impl SpannerDb {
             "name" => name.to_string(),
             "collection_id" => id.to_string(),
         })
-        .execute(&self.conn)?;
+        .execute_dml(&self.conn)?;
         Ok(id)
     }
 
@@ -521,7 +521,7 @@ impl SpannerDb {
                 "pretouch_ts" => TypeCode::TIMESTAMP,
             })
             .execute(&self.conn)?
-            .map(|row| {
+            .map_and_then(|row| {
                 let collection_id = row[0]
                     .get_string_value()
                     .parse::<i32>()
@@ -566,7 +566,7 @@ impl SpannerDb {
                 "ids".to_owned(),
                 as_list_value(uncached.into_iter().map(|id| id.to_string())),
             );
-            let result = self
+            let rs = self
                 .sql(
                     "SELECT collection_id, name
                        FROM collections
@@ -574,7 +574,8 @@ impl SpannerDb {
                 )?
                 .params(params)
                 .execute(&self.conn)?;
-            for row in result {
+            for row_result in rs {
+                let row = row_result?;
                 let id = row[0]
                     .get_string_value()
                     .parse::<i32>()
@@ -608,7 +609,7 @@ impl SpannerDb {
                 "fxa_kid" => user_id.fxa_kid,
             })
             .execute(&self.conn)?
-            .map(|row| {
+            .map_and_then(|row| {
                 let collection_id = row[0]
                     .get_string_value()
                     .parse::<i32>()
@@ -641,7 +642,7 @@ impl SpannerDb {
                 "fxa_kid" => user_id.fxa_kid
             })
             .execute(&self.conn)?
-            .map(|row| {
+            .map_and_then(|row| {
                 let collection_id = row[0]
                     .get_string_value()
                     .parse::<i32>()
@@ -735,7 +736,7 @@ impl SpannerDb {
         )?
         .params(params.clone())
         .param_types(types.clone())
-        .execute(&self.conn)?;
+        .execute_dml(&self.conn)?;
 
         self.sql(
             "INSERT INTO user_collections (fxa_uid, fxa_kid, collection_id, modified)
@@ -743,7 +744,7 @@ impl SpannerDb {
         )?
         .params(params)
         .param_types(types)
-        .execute(&self.conn)?;
+        .execute_dml(&self.conn)?;
         // Return timestamp, because sometimes there's a delay between writing and
         // reading the database.
         Ok(self.timestamp()?)
@@ -761,7 +762,7 @@ impl SpannerDb {
             "fxa_uid" => user_id.fxa_uid,
             "fxa_kid" => user_id.fxa_kid,
         })
-        .execute(&self.conn)?;
+        .execute_dml(&self.conn)?;
         Ok(())
     }
 
@@ -778,7 +779,7 @@ impl SpannerDb {
     ) -> Result<results::DeleteCollection> {
         // Also deletes child bsos/batch rows (INTERLEAVE IN PARENT
         // user_collections ON DELETE CASCADE)
-        let rs = self
+        let affected_rows = self
             .sql(
                 "DELETE FROM user_collections
                   WHERE fxa_uid = @fxa_uid
@@ -795,8 +796,8 @@ impl SpannerDb {
             .param_types(param_types! {
                 "pretouch_ts" => TypeCode::TIMESTAMP,
             })
-            .execute(&self.conn)?;
-        if rs.affected_rows()? > 0 {
+            .execute_dml(&self.conn)?;
+        if affected_rows > 0 {
             self.erect_tombstone(&params.user_id)
         } else {
             self.get_storage_timestamp_sync(params.user_id)
@@ -818,8 +819,9 @@ impl SpannerDb {
         // buffered on the client side and only issued to Spanner in the final
         // transaction Commit.
         let timestamp = self.timestamp()?;
-        if self.session.borrow().touched_collection {
-            // No need to touch it again
+        if !cfg!(any(test, feature = "db_test")) && self.session.borrow().touched_collection {
+            // No need to touch it again (except during tests where we
+            // currently reuse Dbs for multiple requests)
             return Ok(timestamp);
         }
 
@@ -855,7 +857,7 @@ impl SpannerDb {
             )?
             .params(sqlparams)
             .param_types(sql_types)
-            .execute(&self.conn)?;
+            .execute_dml(&self.conn)?;
         } else {
             self.sql(
                 "INSERT INTO user_collections (fxa_uid, fxa_kid, collection_id, modified)
@@ -863,7 +865,7 @@ impl SpannerDb {
             )?
             .params(sqlparams)
             .param_types(sql_types)
-            .execute(&self.conn)?;
+            .execute_dml(&self.conn)?;
         }
         self.session.borrow_mut().touched_collection = true;
         Ok(timestamp)
@@ -872,7 +874,7 @@ impl SpannerDb {
     pub fn delete_bso_sync(&self, params: params::DeleteBso) -> Result<results::DeleteBso> {
         let collection_id = self.get_collection_id(&params.collection)?;
         let touch = self.touch_collection(&params.user_id, collection_id)?;
-        let result = self
+        let affected_rows = self
             .sql(
                 "DELETE FROM bsos
                   WHERE fxa_uid = @fxa_uid
@@ -886,8 +888,8 @@ impl SpannerDb {
                 "collection_id" => collection_id.to_string(),
                 "bso_id" => params.id,
             })
-            .execute(&self.conn)?;
-        if result.affected_rows()? == 0 {
+            .execute_dml(&self.conn)?;
+        if affected_rows == 0 {
             Err(DbErrorKind::BsoNotFound)?
         } else {
             Ok(touch)
@@ -912,11 +914,15 @@ impl SpannerDb {
                 AND bso_id IN UNNEST(@ids)",
         )?
         .params(sqlparams)
-        .execute(&self.conn)?;
+        .execute_dml(&self.conn)?;
         self.touch_collection(&params.user_id, collection_id)
     }
 
-    fn bsos_query_sync(&self, query_str: &str, params: params::GetBsos) -> Result<SyncResultSet> {
+    fn bsos_query_sync(
+        &self,
+        query_str: &str,
+        params: params::GetBsos,
+    ) -> Result<StreamedResultSet> {
         let mut query = query_str.to_owned();
         let mut sqlparams = params! {
             "fxa_uid" => params.user_id.fxa_uid,
@@ -995,11 +1001,10 @@ impl SpannerDb {
         let limit = params.params.limit.map(i64::from).unwrap_or(-1);
         let offset = params.params.offset.unwrap_or(0) as i64;
 
-        let result: Result<Vec<_>> = self
+        let mut bsos = self
             .bsos_query_sync(query, params)?
-            .map(bso_from_row)
-            .collect();
-        let mut bsos: Vec<_> = result?;
+            .map_and_then(bso_from_row)
+            .collect::<Result<Vec<_>>>()?;
 
         // NOTE: when bsos.len() == 0, server-syncstorage (the Python impl)
         // makes an additional call to get_collection_timestamp to potentially
@@ -1033,10 +1038,10 @@ impl SpannerDb {
                AND collection_id = @collection_id
                AND expiry > CURRENT_TIMESTAMP()";
 
-        let mut ids: Vec<String> = self
+        let mut ids = self
             .bsos_query_sync(query, params)?
-            .map(|r| r[0].get_string_value().to_owned())
-            .collect();
+            .map_results(|row| row[0].get_string_value().to_owned())
+            .collect::<Result<Vec<_>>>()?;
 
         // NOTE: when bsos.len() == 0, server-syncstorage (the Python impl)
         // makes an additional call to get_collection_timestamp to potentially
@@ -1060,15 +1065,14 @@ impl SpannerDb {
 
     pub fn get_bso_sync(&self, params: params::GetBso) -> Result<Option<results::GetBso>> {
         let collection_id = self.get_collection_id(&params.collection)?;
-
         self.sql(
             "SELECT bso_id, sortindex, payload, modified, expiry
-                   FROM bsos
-                  WHERE fxa_uid = @fxa_uid
-                    AND fxa_kid = @fxa_kid
-                    AND collection_id = @collection_id
-                    AND bso_id = @bso_id
-                    AND expiry > CURRENT_TIMESTAMP()",
+               FROM bsos
+              WHERE fxa_uid = @fxa_uid
+                AND fxa_kid = @fxa_kid
+                AND collection_id = @collection_id
+                AND bso_id = @bso_id
+                AND expiry > CURRENT_TIMESTAMP()",
         )?
         .params(params! {
             "fxa_uid" => params.user_id.fxa_uid,
@@ -1145,7 +1149,7 @@ impl SpannerDb {
             "ids".to_owned(),
             as_list_value(params.bsos.iter().map(|pbso| pbso.id.clone())),
         );
-        let existing: Vec<_> = self
+        let existing = self
             .sql(
                 "SELECT bso_id
                    FROM bsos
@@ -1156,8 +1160,8 @@ impl SpannerDb {
             )?
             .params(sqlparams)
             .execute(&self.conn)?
-            .map(|row| row[0].get_string_value().to_owned())
-            .collect();
+            .map_results(|row| row[0].get_string_value().to_owned())
+            .collect::<Result<Vec<_>>>()?;
 
         let mut inserts = vec![];
         let mut updates = HashMap::new();
@@ -1368,7 +1372,8 @@ impl SpannerDb {
         self.sql(&sql)?
             .params(sqlparams)
             .param_types(sqltypes)
-            .execute(&self.conn)?;
+            .execute_dml(&self.conn)?;
+
         Ok(touch)
     }
 

src/db/spanner/support.rs
@@ -1,11 +1,16 @@
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt, mem,
+    result::Result as StdResult,
+};
 
+use futures::stream::{Stream, Wait};
 use googleapis_raw::spanner::v1::{
-    result_set::{ResultSet, ResultSetMetadata, ResultSetStats},
+    result_set::{PartialResultSet, ResultSetMetadata, ResultSetStats},
     spanner::ExecuteSqlRequest,
     type_pb::{StructType_Field, Type, TypeCode},
 };
-
+use grpcio::ClientSStreamReceiver;
 use protobuf::{
     well_known_types::{ListValue, NullValue, Struct, Value},
     RepeatedField,
@@ -82,9 +87,9 @@ impl ExecuteSqlRequestBuilder {
         self
     }
 
-    pub fn execute(self, spanner: &Conn) -> Result<SyncResultSet> {
+    fn prepare_request(self, conn: &Conn) -> ExecuteSqlRequest {
         let mut request = self.execute_sql;
-        request.set_session(spanner.session.get_name().to_owned());
+        request.set_session(conn.session.get_name().to_owned());
         if let Some(params) = self.params {
             let mut paramss = Struct::new();
             paramss.set_fields(params);
@@ -93,24 +98,71 @@ impl ExecuteSqlRequestBuilder {
         if let Some(param_types) = self.param_types {
             request.set_param_types(param_types);
         }
-        let result = spanner.client.execute_sql(&request)?;
-        Ok(SyncResultSet { result })
+        request
+    }
+
+    /// Execute a SQL read statement
+    pub fn execute(self, conn: &Conn) -> Result<StreamedResultSet> {
+        let stream = conn
+            .client
+            .execute_streaming_sql(&self.prepare_request(conn))?;
+        Ok(StreamedResultSet::new(stream))
+    }
+
+    /// Execute a DML statement, returning the exact count of modified rows
+    pub fn execute_dml(self, conn: &Conn) -> Result<i64> {
+        let rs = conn.client.execute_sql(&self.prepare_request(conn))?;
+        Ok(rs.get_stats().get_row_count_exact())
     }
 }
 
-#[derive(Debug)]
-pub struct SyncResultSet {
-    result: ResultSet,
+/// Streams results from an ExecuteStreamingSql PartialResultSet
+///
+/// Utilizies futures 0.1 `wait()`, so should *always* be called from a thread
+/// outside of an event loop
+pub struct StreamedResultSet {
+    /// Stream from execute_streaming_sql
+    stream: Wait<ClientSStreamReceiver<PartialResultSet>>,
+
+    metadata: Option<ResultSetMetadata>,
+    stats: Option<ResultSetStats>,
+
+    /// Fully-processed rows
+    rows: VecDeque<Vec<Value>>,
+    /// Accumulated values for incomplete row
+    current_row: Vec<Value>,
+    /// Incomplete value
+    pending_chunk: Option<Value>,
 }
 
-impl SyncResultSet {
+impl StreamedResultSet {
+    pub fn new(stream: ClientSStreamReceiver<PartialResultSet>) -> Self {
+        Self {
+            // Note: wait() isn't futures 0.3 compatible
+            stream: stream.wait(),
+            metadata: None,
+            stats: None,
+            rows: Default::default(),
+            current_row: vec![],
+            pending_chunk: None,
+        }
+    }
+
     #[allow(dead_code)]
     pub fn metadata(&self) -> Option<&ResultSetMetadata> {
-        self.result.metadata.as_ref()
+        self.metadata.as_ref()
     }
 
+    #[allow(dead_code)]
     pub fn stats(&self) -> Option<&ResultSetStats> {
-        self.result.stats.as_ref()
+        self.stats.as_ref()
+    }
+
+    pub fn fields(&self) -> &[StructType_Field] {
+        match self.metadata {
+            Some(ref metadata) => metadata.get_row_type().get_fields(),
+            None => &[],
+        }
     }
 
     pub fn one(&mut self) -> Result<Vec<Value>> {
@@ -128,36 +180,136 @@ impl SyncResultSet {
         } else if self.next().is_some() {
             Err(DbError::internal("Execpted one result; got more."))?
         } else {
-            Ok(result)
+            result.transpose()
         }
     }
 
-    pub fn affected_rows(self: &SyncResultSet) -> Result<i64> {
-        let stats = self
-            .stats()
-            .ok_or_else(|| DbError::internal("Expected result_set stats"))?;
-        let row_count_exact = stats.get_row_count_exact();
-        Ok(row_count_exact)
+    /// Pull and process the next values from the Stream
+    ///
+    /// Returns false when the stream is finished
+    fn consume_next(&mut self) -> Result<bool> {
+        let mut partial_rs = if let Some(result) = self.stream.next() {
+            result?
+        } else {
+            // Stream finished
+            return Ok(false);
+        };
+
+        if self.metadata.is_none() && partial_rs.has_metadata() {
+            // first response
+            self.metadata = Some(partial_rs.take_metadata());
+        }
+        if partial_rs.has_stats() {
+            // last response
+            self.stats = Some(partial_rs.take_stats());
+        }
+
+        let mut values = partial_rs.take_values().into_vec();
+        if values.is_empty() {
+            // sanity check
+            return Ok(true);
+        }
+
+        if let Some(pending_chunk) = self.pending_chunk.take() {
+            let fields = self.fields();
+            let current_row_i = self.current_row.len();
+            if fields.len() <= current_row_i {
+                Err(DbErrorKind::Integrity(
+                    "Invalid PartialResultSet fields".to_owned(),
+                ))?;
+            }
+            let field = &fields[current_row_i];
+            values[0] = merge_by_type(pending_chunk, &values[0], field.get_field_type())?;
+        }
+        if partial_rs.get_chunked_value() {
+            self.pending_chunk = values.pop();
+        }
+
+        self.consume_values(values);
+        Ok(true)
+    }
+
+    fn consume_values(&mut self, values: Vec<Value>) {
+        let width = self.fields().len();
+        for value in values {
+            self.current_row.push(value);
+            if self.current_row.len() == width {
+                let current_row = mem::replace(&mut self.current_row, vec![]);
+                self.rows.push_back(current_row);
+            }
+        }
     }
 }
 
-impl Iterator for SyncResultSet {
-    type Item = Vec<Value>;
+/// Iteration around the result stream
+///
+/// `Item` is a Result as errors may happen
+/// mid-stream. `itertools::IterTools::map_results` and
+/// `MapAndThenIterator::map_and_then` can aid in removing boilerplate around
+/// handling of said Results
+impl Iterator for StreamedResultSet {
+    type Item = Result<Vec<Value>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let rows = &mut self.result.rows;
-        if rows.is_empty() {
-            None
-        } else {
-            let row = rows.remove(0);
-            Some(row.get_values().to_vec())
+        while self.rows.is_empty() {
+            match self.consume_next() {
+                Ok(true) => (),
+                Ok(false) => return None,
+                // Note: Iteration may continue after an error. We may want to
+                // stop afterwards instead for safety sake (it's not really
+                // recoverable)
+                Err(e) => return Some(Err(e)),
+            }
         }
+        Ok(self.rows.pop_front()).transpose()
     }
+}
 
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.result.rows.len();
-        (len, Some(len))
+impl fmt::Debug for StreamedResultSet {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("StreamedResultSet")
+            .field("metadata", &self.metadata)
+            .field("stats", &self.stats)
+            .field("rows", &self.rows)
+            .field("current_row", &self.current_row)
+            .field("pending_chunk", &self.pending_chunk)
+            .finish()
     }
+}
+
+fn merge_by_type(lhs: Value, rhs: &Value, field_type: &Type) -> Result<Value> {
+    // We only support merging basic string types as that's all we currently use.
+    // The python client also supports: float64, array, struct. The go client
+    // only additionally supports array (claiming structs are only returned as
+    // arrays anyway)
+    match field_type.get_code() {
+        TypeCode::BYTES
+        | TypeCode::DATE
+        | TypeCode::INT64
+        | TypeCode::STRING
+        | TypeCode::TIMESTAMP => merge_string(lhs, rhs),
+        TypeCode::ARRAY
+        | TypeCode::FLOAT64
+        | TypeCode::STRUCT
+        | TypeCode::TYPE_CODE_UNSPECIFIED
+        | TypeCode::BOOL => unsupported_merge(field_type),
+    }
+}
+
+fn unsupported_merge(field_type: &Type) -> Result<Value> {
+    Err(DbError::internal(&format!(
+        "merge not supported, type: {:?}",
+        field_type
+    )))
+}
+
+fn merge_string(mut lhs: Value, rhs: &Value) -> Result<Value> {
+    if !lhs.has_string_value() || !rhs.has_string_value() {
+        Err(DbError::internal("merge_string has no string value"))?
+    }
+    let mut merged = lhs.take_string_value();
+    merged.push_str(rhs.get_string_value());
+    Ok(as_value(merged))
 }
 
 pub fn bso_from_row(row: Vec<Value>) -> Result<results::GetBso> {
@@ -245,3 +397,40 @@ pub fn bso_to_update_row(
     row.set_values(RepeatedField::from_vec(values));
     Ok((columns, row))
 }
+
+#[derive(Clone)]
+pub struct MapAndThenIterator<I, F> {
+    iter: I,
+    f: F,
+}
+
+impl<A, B, E, I, F> Iterator for MapAndThenIterator<I, F>
+where
+    F: FnMut(A) -> StdResult<B, E>,
+    I: Iterator<Item = StdResult<A, E>>,
+{
+    type Item = StdResult<B, E>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|result| result.and_then(&mut self.f))
+    }
+}
+
+pub trait MapAndThenTrait {
+    /// Return an iterator adaptor that applies the provided closure to every
+    /// Result::Ok value. Result::Err values are unchanged.
+    ///
+    /// The closure can be used for control flow based on result values
+    fn map_and_then<F, A, B, E>(self, func: F) -> MapAndThenIterator<Self, F>
+    where
+        Self: Sized + Iterator<Item = StdResult<A, E>>,
+        F: FnMut(A) -> StdResult<B, E>,
+    {
+        MapAndThenIterator {
+            iter: self,
+            f: func,
+        }
+    }
+}
+
+impl<I, T, E> MapAndThenTrait for I where I: Sized + Iterator<Item = StdResult<T, E>> {}