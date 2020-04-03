8b951137374218ac6d2ec23e5f2c975b45fc2105
Eman <emmanuelgatwech@gmail.com>
2020-04-03 13:36:59 +0300

    fix: convert user_id into bigint
    The MySQL schema defines userid columns as 32 bit integers inteded for use by the token server which expects 64 bit integers.
    This change creates a new migration to convert the columns and to modify the `src/db/mysql/schema.rs` to reflect those changes.
    
    Closes #470

---
+++ migrations/2020-04-03-102015_change_userid/up.sql
@@ -0,0 +1,4 @@
+-- Your SQL goes here
+alter table `batches` change column `userid` `userid` bigint(20) not null;
+alter table `bso` change column `userid` `userid` bigint(20) not null;
+alter table `user_collections` change column `userid` `userid` bigint(20) not null;
