02e8bcbf2caebc5baef94a69a6f7fb569e9edd2f
Mark Drobnak <mdrobnak@mozilla.com>
2020-07-02 13:09:53 -0400

    Use async version of get_session in SpannerConnectionManager::is_valid

--- src/db/spanner/manager.rs
+++ src/db/spanner/manager.rs
@@ -95,7 +95,7 @@ impl<T: std::marker::Send + std::marker::Sync + 'static> ManageConnection
     async fn is_valid(&self, conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
         let mut req = GetSessionRequest::new();
         req.set_name(conn.session.get_name().to_owned());
-        if let Err(e) = conn.client.get_session(&req) {
+        if let Err(e) = conn.client.get_session_async(&req)?.await {
             return Err(e);
         }
         Ok(conn)
