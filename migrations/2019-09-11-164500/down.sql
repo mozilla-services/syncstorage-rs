a3d50434a0b3d2c7ec368d59f735562ad7dba180
jrconlin <jr+git@mozilla.com>
2020-07-10 19:44:29 +0000

    feat: make migrations play nice with existing databases.
    Closes #663

--- migrations/2019-09-11-164500/down.sql
+++ migrations/2019-09-11-164500/down.sql
@@ -1,3 +1,8 @@
+-- At this point, it's sanest to just drop the tables rather than revert them
+-- there are a number of non-backwards compatible changes performed and data
+-- corruption is HIGHLY likely.
+-- Best just try and install the python version (probably in a docker), and
+-- let the client try and reconnect and restore.
 DROP TABLE IF EXISTS `bso`;
 DROP TABLE IF EXISTS `collections`;
 DROP TABLE IF EXISTS `user_collections`;
--- migrations/2019-09-11-164500/up.sql
+++ migrations/2019-09-11-164500/up.sql
@@ -1,11 +1,54 @@
-alter table `batches` change column `user_id` `userid` int(11) not null;
-alter table `batches` change column `collection_id` `collection` int(11) not null;
-alter table `bso` change column `user_id` `userid` int(11) not null;
-alter table `bso` change column `collection_id` `collection` int(11) not null;
-alter table `bso` change column `expiry` `ttl` bigint(20) not null;
-alter table `user_collections` change column `user_id` `userid` int(11) not null;
-alter table `user_collections` change column `collection_id` `collection` int(11) not null;
-alter table `user_collections` change column `modified` `last_modified` bigint(20) not null;
--- must be last in case of error
--- the following column is not used, but preserved for legacy and stand alone systems.
-alter table `bso` add column `payload_size` int(11) default 0;
+-- tests to see if columns need adjustment
+-- this is because the `init` may create the tables
+-- with column names already correct
+
+CREATE PROCEDURE UPDATE_165600()
+BEGIN
+    IF EXISTS( SELECT column_name
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name='bso' AND column_name="user_id")
+    THEN
+        BEGIN
+            alter table `bso` change column `user_id` `userid` int(11) not null;
+            alter table `bso` change column `collection_id` `collection` int(11) not null;
+            alter table `bso` change column `expiry` `ttl` bigint(20) not null;
+        END;
+    END IF;
+
+    IF EXISTS( SELECT column_name
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name='batches' AND column_name="user_id")
+    THEN
+        BEGIN
+            alter table `batches` change column `user_id` `userid` int(11) not null;
+            alter table `batches` change column `collection_id` `collection` int(11) not null;
+        END;
+    END IF;
+
+    IF EXISTS( SELECT column_name
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name='user_collections' AND column_name="user_id")
+    THEN
+        BEGIN
+            alter table `user_collections` change column `user_id` `userid` int(11) not null;
+            alter table `user_collections` change column `collection_id` `collection` int(11) not null;
+            alter table `user_collections` change column `modified` `last_modified` bigint(20) not null;
+        END;
+    END IF;
+
+    -- must be last in case of error
+    -- the following column is not used, but preserved for legacy and stand alone systems.
+    IF NOT EXISTS( SELECT column_name
+        FROM INFORMATION_SCHEMA.COLUMNS
+        where table_name='bso' AND column_name="payload_size")
+    THEN
+        BEGIN
+            alter table `bso` add column `payload_size` int(11) default 0;
+        END;
+
+    END IF;
+END;
+
+CALL UPDATE_165600();
+
+DROP PROCEDURE UPDATE_165600;
