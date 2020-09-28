6b87d1905fc49279fb901d91e26193cca5f71b87
Philip Jenvey <pjenvey@underboss.org>
2020-09-28 16:32:58 -0700

    clippy

--- src/web/middleware/rejectua.rs
+++ src/web/middleware/rejectua.rs
@@ -75,20 +75,11 @@ where
     fn call(&mut self, sreq: ServiceRequest) -> Self::Future {
         match sreq.headers().get(USER_AGENT) {
             Some(header) if header.to_str().map_or(false, should_reject) => {
-                let state = match &sreq.app_data::<Data<ServerState>>() {
-                    Some(v) => v.clone(),
-                    None => {
-                        return Either::Left(future::ok(
-                            sreq.into_response(
-                                HttpResponse::InternalServerError()
-                                    .body("Err: No State".to_owned())
-                                    .into_body(),
-                            ),
-                        ))
-                    }
-                };
+                let data = sreq
+                    .app_data::<Data<ServerState>>()
+                    .expect("No app_data ServerState");
                 debug!("Rejecting User-Agent: {:?}", header);
-                Metrics::from(state.as_ref()).incr("error.rejectua");
+                Metrics::from(data.get_ref()).incr("error.rejectua");
 
                 Either::Left(future::ok(
                     sreq.into_response(
