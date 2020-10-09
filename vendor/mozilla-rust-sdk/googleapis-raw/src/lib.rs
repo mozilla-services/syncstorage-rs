c6f9cf9bd4ef7bff13ddc33a71f5771dd9bf6ea3
jrconlin <jr+git@mozilla.com>
2020-10-09 09:34:40 -0700

    chore: update to protobuf 2.18.0
    Closes #852

--- vendor/mozilla-rust-sdk/googleapis-raw/src/lib.rs
+++ vendor/mozilla-rust-sdk/googleapis-raw/src/lib.rs
@@ -3,7 +3,7 @@
 // This appears as a comment in each generated file. Add it once here
 // to save a bit of time and effort.
 
-const _PROTOBUF_VERSION_CHECK: () = ::protobuf::VERSION_2_17_0;
+const _PROTOBUF_VERSION_CHECK: () = ::protobuf::VERSION_2_18_0;
 
 pub mod empty;
 pub(crate) mod iam;
