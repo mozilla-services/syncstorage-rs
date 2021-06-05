8ea987adfc31ba8bbe3e4a93bc46167b0bf4f89b
jrconlin <jr+git@mozilla.com>
2021-06-05 12:58:28 -0700

    f ignore

--- vendor/mozilla-rust-sdk/googleapis-raw/src/longrunning/operations_grpc.rs
+++ vendor/mozilla-rust-sdk/googleapis-raw/src/longrunning/operations_grpc.rs
@@ -148,11 +148,21 @@ impl OperationsClient {
 }
 
 pub trait Operations {
-    fn list_operations(&mut self, ctx: ::grpcio::RpcContext, req: super::operations::ListOperationsRequest, sink: ::grpcio::UnarySink<super::operations::ListOperationsResponse>);
-    fn get_operation(&mut self, ctx: ::grpcio::RpcContext, req: super::operations::GetOperationRequest, sink: ::grpcio::UnarySink<super::operations::Operation>);
-    fn delete_operation(&mut self, ctx: ::grpcio::RpcContext, req: super::operations::DeleteOperationRequest, sink: ::grpcio::UnarySink<super::empty::Empty>);
-    fn cancel_operation(&mut self, ctx: ::grpcio::RpcContext, req: super::operations::CancelOperationRequest, sink: ::grpcio::UnarySink<super::empty::Empty>);
-    fn wait_operation(&mut self, ctx: ::grpcio::RpcContext, req: super::operations::WaitOperationRequest, sink: ::grpcio::UnarySink<super::operations::Operation>);
+    fn list_operations(&mut self, ctx: ::grpcio::RpcContext, _req: super::operations::ListOperationsRequest, sink: ::grpcio::UnarySink<super::operations::ListOperationsResponse>) {
+        grpcio::unimplemented_call!(ctx, sink)
+    }
+    fn get_operation(&mut self, ctx: ::grpcio::RpcContext, _req: super::operations::GetOperationRequest, sink: ::grpcio::UnarySink<super::operations::Operation>) {
+        grpcio::unimplemented_call!(ctx, sink)
+    }
+    fn delete_operation(&mut self, ctx: ::grpcio::RpcContext, _req: super::operations::DeleteOperationRequest, sink: ::grpcio::UnarySink<super::empty::Empty>) {
+        grpcio::unimplemented_call!(ctx, sink)
+    }
+    fn cancel_operation(&mut self, ctx: ::grpcio::RpcContext, _req: super::operations::CancelOperationRequest, sink: ::grpcio::UnarySink<super::empty::Empty>) {
+        grpcio::unimplemented_call!(ctx, sink)
+    }
+    fn wait_operation(&mut self, ctx: ::grpcio::RpcContext, _req: super::operations::WaitOperationRequest, sink: ::grpcio::UnarySink<super::operations::Operation>) {
+        grpcio::unimplemented_call!(ctx, sink)
+    }
 }
 
 pub fn create_operations<S: Operations + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
