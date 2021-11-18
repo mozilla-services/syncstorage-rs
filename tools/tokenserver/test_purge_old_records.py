0ac30958de5cfca0f3d44dfb479b615cae7ede27
Ethan Donowitz <8703826+ethowitz@users.noreply.github.com>
2021-11-18 09:37:02 -0500

    feat: add Tokenserver admin scripts (#1168)
    Closes #1086

---
+++ tools/tokenserver/test_purge_old_records.py
@@ -0,0 +1,136 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import hawkauthlib
+import re
+import threading
+import tokenlib
+import unittest
+from wsgiref.simple_server import make_server
+
+from database import Database
+from purge_old_records import purge_old_records
+
+
+class TestPurgeOldRecords(unittest.TestCase):
+    """A testcase for proper functioning of the purge_old_records.py script.
+
+    This is a tricky one, because we have to actually run the script and
+    test that it does the right thing.  We also run a mock downstream service
+    so we can test that data-deletion requests go through ok.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.service_requests = []
+        cls.service_node = "http://localhost:8002"
+        cls.service = make_server("localhost", 8002, cls._service_app)
+        target = cls.service.serve_forever
+        cls.service_thread = threading.Thread(target=target)
+        # Note: If the following `start` causes the test thread to hang,
+        # you may need to specify
+        # `[app::pyramid.app] pyramid.worker_class = sync` in the test_*.ini
+        # files
+        cls.service_thread.start()
+        # This silences nuisance on-by-default logging output.
+        cls.service.RequestHandlerClass.log_request = lambda *a: None
+
+    def setUp(self):
+        super(TestPurgeOldRecords, self).setUp()
+
+        # Configure the node-assignment backend to talk to our test service.
+        self.database = Database()
+        self.database.add_node(self.service_node, 100)
+
+    def tearDown(self):
+        cursor = self.database._execute_sql('DELETE FROM nodes')
+        cursor.close()
+
+        cursor = self.database._execute_sql('DELETE FROM users')
+        cursor.close()
+
+        del self.service_requests[:]
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.service.shutdown()
+        cls.service_thread.join()
+
+    @classmethod
+    def _service_app(cls, environ, start_response):
+        cls.service_requests.append(environ)
+        start_response("200 OK", [])
+        return ""
+
+    def test_purging_of_old_user_records(self):
+        # Make some old user records.
+        email = "test@mozilla.com"
+        user = self.database.allocate_user(email, client_state="aa",
+                                           generation=123)
+        self.database.update_user(user, client_state="bb",
+                                  generation=456, keys_changed_at=450)
+        self.database.update_user(user, client_state="cc",
+                                  generation=789)
+        user_records = list(self.database.get_user_records(email))
+        self.assertEqual(len(user_records), 3)
+        user = self.database.get_user(email)
+        self.assertEquals(user["client_state"], "cc")
+        self.assertEquals(len(user["old_client_states"]), 2)
+
+        # The default grace-period should prevent any cleanup.
+        node_secret = "SECRET"
+        self.assertTrue(purge_old_records(node_secret))
+        user_records = list(self.database.get_user_records(email))
+        self.assertEqual(len(user_records), 3)
+        self.assertEqual(len(self.service_requests), 0)
+
+        # With no grace period, we should cleanup two old records.
+        self.assertTrue(purge_old_records(node_secret, grace_period=0))
+        user_records = list(self.database.get_user_records(email))
+        self.assertEqual(len(user_records), 1)
+        self.assertEqual(len(self.service_requests), 2)
+
+        # Check that the proper delete requests were made to the service.
+        expected_kids = ["0000000000450-uw", "0000000000123-qg"]
+        for i, environ in enumerate(self.service_requests):
+            # They must be to the correct path.
+            self.assertEquals(environ["REQUEST_METHOD"], "DELETE")
+            self.assertTrue(re.match("/1.5/[0-9]+", environ["PATH_INFO"]))
+            # They must have a correct request signature.
+            token = hawkauthlib.get_id(environ)
+            secret = tokenlib.get_derived_secret(token, secret=node_secret)
+            self.assertTrue(hawkauthlib.check_signature(environ, secret))
+            userdata = tokenlib.parse_token(token, secret=node_secret)
+            self.assertTrue("uid" in userdata)
+            self.assertTrue("node" in userdata)
+            self.assertEqual(userdata["fxa_uid"], "test")
+            self.assertEqual(userdata["fxa_kid"], expected_kids[i])
+
+        # Check that the user's current state is unaffected
+        user = self.database.get_user(email)
+        self.assertEquals(user["client_state"], "cc")
+        self.assertEquals(len(user["old_client_states"]), 0)
+
+    def test_purging_is_not_done_on_downed_nodes(self):
+        # Make some old user records.
+        node_secret = "SECRET"
+        email = "test@mozilla.com"
+        user = self.database.allocate_user(email, client_state="aa")
+        self.database.update_user(user, client_state="bb")
+        user_records = list(self.database.get_user_records(email))
+        self.assertEqual(len(user_records), 2)
+
+        # With the node down, we should not purge any records.
+        self.database.update_node(self.service_node, downed=1)
+        self.assertTrue(purge_old_records(node_secret, grace_period=0))
+        user_records = list(self.database.get_user_records(email))
+        self.assertEqual(len(user_records), 2)
+        self.assertEqual(len(self.service_requests), 0)
+
+        # With the node back up, we should purge correctly.
+        self.database.update_node(self.service_node, downed=0)
+        self.assertTrue(purge_old_records(node_secret, grace_period=0))
+        user_records = list(self.database.get_user_records(email))
+        self.assertEqual(len(user_records), 1)
+        self.assertEqual(len(self.service_requests), 1)
---
+++ tools/tokenserver/unassign_node.py
@@ -0,0 +1,72 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+Script to remove a node from the system.
+
+This script clears any assignments to the named node.
+
+"""
+
+import logging
+import optparse
+
+from database import Database
+import util
+
+
+logger = logging.getLogger("tokenserver.scripts.unassign_node")
+
+
+def unassign_node(node):
+    """Clear any assignments to the named node."""
+    logger.info("Unassignment node %s", node)
+    try:
+        database = Database()
+        found = False
+        try:
+            database.unassign_node(node)
+        except ValueError:
+            logger.debug("  not found")
+        else:
+            found = True
+            logger.debug("  unassigned")
+    except Exception:
+        logger.exception("Error while unassigning node")
+        return False
+    else:
+        if not found:
+            logger.info("Node %s was not found", node)
+        else:
+            logger.info("Finished unassigning node %s", node)
+        return True
+
+
+def main(args=None):
+    """Main entry-point for running this script.
+
+    This function parses command-line arguments and passes them on
+    to the unassign_node() function.
+    """
+    usage = "usage: %prog [options] node_name"
+    descr = "Clear all assignments to node in the tokenserver database"
+    parser = optparse.OptionParser(usage=usage, description=descr)
+    parser.add_option("-v", "--verbose", action="count", dest="verbosity",
+                      help="Control verbosity of log messages")
+
+    opts, args = parser.parse_args(args)
+    if len(args) != 1:
+        parser.print_usage()
+        return 1
+
+    util.configure_script_logging(opts)
+
+    node_name = args[0]
+
+    unassign_node(node_name)
+    return 0
+
+
+if __name__ == "__main__":
+    util.run_script(main)
