3f9ba369d532886c8c627405e3713975ecc1b168
Ethan Donowitz <edonowitz@mozilla.com>
2022-09-13 16:25:43 +0000

    flake8

--- tools/integration_tests/tokenserver/test_authorization.py
+++ tools/integration_tests/tokenserver/test_authorization.py
@@ -302,15 +302,15 @@ class TestAuthorization(TestCase, unittest.TestCase):
     def test_set_generation_with_keys_changed_at_initialization(self):
         # Add a user who has never sent us a generation or a keys_changed_at
         uid = self._add_user(generation=0, keys_changed_at=None,
-                            client_state='aaaa')
+                             client_state='aaaa')
 
         # Only BrowserID requests can omit keys_changed_at
         if self.auth_method == 'browserid':
             # Send a request without a generation that doesn't update
             # keys_changed_at
             headers = self._build_auth_headers(generation=None,
-                                            keys_changed_at=None,
-                                            client_state='aaaa')
+                                               keys_changed_at=None,
+                                               client_state='aaaa')
             self.app.get('/1.0/sync/1.5', headers=headers)
             user = self._get_user(uid)
             # This should not have set the user's generation
