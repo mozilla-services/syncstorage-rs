0ab86ac6150eda700273250fca04a9fc8522ec04
jrconlin <jr+git@mozilla.com>
2024-01-09 09:48:24 -0800

    bug: Copy modified purge script from old tokenserver  repo

--- tools/tokenserver/purge_old_records.py
+++ tools/tokenserver/purge_old_records.py
@@ -5,7 +5,8 @@
 
 Script to purge user records that have been replaced.
 
-This script purges any obsolete user records from the database.
+This script takes a tokenserver config file, uses it to load the assignment
+backend, and then purges any obsolete user records from that backend.
 Obsolete records are those that have been replaced by a newer record for
 the same user.
 
@@ -15,33 +16,31 @@ overheads, improve performance etc if run regularly.
 
 """
 
-import binascii
-import hawkauthlib
+import os
+import time
+import random
 import logging
 import optparse
-import random
-import requests
-import time
-import tokenlib
 
-import util
-from database import Database
-from util import format_key_id
+import requests
+import tokenlib
+import hawkauthlib
+
+import tokenserver.scripts
+from tokenserver.assignment import INodeAssignment
+from tokenserver.util import format_key_id
 
 
 logger = logging.getLogger("tokenserver.scripts.purge_old_records")
 
-PATTERN = "{node}/1.5/{uid}"
 
+def purge_old_records(config_file, grace_period=-1, max_per_loop=10,
+                      max_offset=0, request_timeout=60, settings=None):
+    """Purge old records from the assignment backend in the given config file.
 
-def purge_old_records(secret, grace_period=-1, max_per_loop=10, max_offset=0,
-                      request_timeout=60):
-    """Purge old records from the database.
-
-    This function queries all of the old user records in the database, deletes
-    the Tokenserver database record for each of the users, and issues a delete
-    request to each user's storage node. The result is a gradual pruning of
-    expired items from each database.
+    This function iterates through each storage backend in the given config
+    file and calls its purge_expired_items() method.  The result is a
+    gradual pruning of expired items from each database.
 
     `max_offset` is used to select a random offset into the list of purgeable
     records. With multiple tasks running concurrently, this will provide each
@@ -49,59 +48,107 @@ def purge_old_records(secret, grace_period=-1, max_per_loop=10, max_offset=0,
     randomization.
     """
     logger.info("Purging old user records")
+    logger.debug("Using config file %r", config_file)
+    config = tokenserver.scripts.load_configurator(config_file)
+    config.begin()
     try:
-        database = Database()
-        # Process batches of <max_per_loop> items, until we run out.
-        while True:
-            offset = random.randint(0, max_offset)
-            kwds = {
-                "grace_period": grace_period,
-                "limit": max_per_loop,
-                "offset": offset,
-            }
-            rows = list(database.get_old_user_records(**kwds))
-            logger.info("Fetched %d rows at offset %d", len(rows), offset)
-            for row in rows:
-                # Don't attempt to purge data from downed nodes.
-                # Instead wait for them to either come back up or to be
-                # completely removed from service.
-                if row.node is None:
-                    logger.info("Deleting user record for uid %s on %s",
-                                row.uid, row.node)
-                    database.delete_user_record(row.uid)
-                elif not row.downed:
-                    logger.info("Purging uid %s on %s", row.uid, row.node)
-                    delete_service_data(row, secret, timeout=request_timeout)
-                    database.delete_user_record(row.uid)
-            if len(rows) < max_per_loop:
-                break
-    except Exception:
-        logger.exception("Error while purging old user records")
+        backend = config.registry.getUtility(INodeAssignment)
+        patterns = config.registry['endpoints_patterns']
+        for service in patterns:
+            previous_list = []
+            logger.debug("Purging old user records for service: %s", service)
+            # Process batches of <max_per_loop> items, until we run out.
+            while True:
+                offset = random.randint(0, max_offset)
+                kwds = {
+                    "grace_period": grace_period,
+                    "limit": max_per_loop,
+                    "offset": offset,
+                }
+                rows = list(backend.get_old_user_records(service, **kwds))
+                if not rows:
+                    logger.info("No more data for %s", service)
+                    break
+                if rows == previous_list:
+                    raise Exception("Loop detected")
+                previous_list = rows
+                logger.info("Fetched %d rows at offset %d", len(rows), offset)
+                counter = 0
+                for row in rows:
+                    # Don't attempt to purge data from downed nodes.
+                    # Instead wait for them to either come back up or to be
+                    # completely removed from service.
+                    if row.node is None:
+                        logger.info("Deleting user record for uid %s on %s",
+                                    row.uid, row.node)
+                        if settings and not settings.dryrun:
+                            backend.delete_user_record(service, row.uid)
+                    elif not row.downed:
+                        logger.info("Purging uid %s on %s", row.uid, row.node)
+                        if settings and not settings.dryrun:
+                            delete_service_data(config, service, row,
+                                                timeout=request_timeout,
+                                                settings=settings)
+                            backend.delete_user_record(service, row.uid)
+                        counter += 1
+                    elif settings and settings.force:
+                        logger.info(
+                            "Forcing tokenserver record delete: {}".format(
+                                row.uid
+                            )
+                        )
+                        logger.info("Forcing Purge uid %s on %s",
+                                    row.uid,
+                                    row.node)
+                        if not settings.dryrun:
+                            delete_service_data(config, service, row,
+                                                timeout=request_timeout,
+                                                settings=settings)
+                            backend.delete_user_record(service, row.uid)
+                        counter += 1
+                    if settings and settings.max_records:
+                        if counter >= settings.max_records:
+                            logger.info("Reached max_records, exiting")
+                            return True
+                if len(rows) < max_per_loop:
+                    break
+    except Exception as e:
+        logger.exception("Error while purging old user records: {}".format(e))
         return False
     else:
         logger.info("Finished purging old user records")
         return True
+    finally:
+        config.end()
 
 
-def delete_service_data(user, secret, timeout=60):
+def delete_service_data(config, service, user, timeout=60, settings=None):
     """Send a data-deletion request to the user's service node.
 
     This is a little bit of hackery to cause the user's service node to
     remove any data it still has stored for the user.  We simulate a DELETE
     request from the user's own account.
     """
+    secrets = config.registry.settings['tokenserver.secrets']
+    pattern = config.registry['endpoints_patterns'][service]
+    node_secrets = secrets.get(user.node)
+    if not node_secrets:
+        msg = "The node %r does not have any shared secret" % (user.node,)
+        raise ValueError(msg)
     token = tokenlib.make_token({
         "uid": user.uid,
         "node": user.node,
         "fxa_uid": user.email.split("@", 1)[0],
         "fxa_kid": format_key_id(
             user.keys_changed_at or user.generation,
-            binascii.unhexlify(user.client_state)
+            user.client_state.decode('hex')
         ),
-    }, secret=secret)
-    secret = tokenlib.get_derived_secret(token, secret=secret)
-    endpoint = PATTERN.format(uid=user.uid, node=user.node)
+    }, secret=node_secrets[-1])
+    secret = tokenlib.get_derived_secret(token, secret=node_secrets[-1])
+    endpoint = pattern.format(uid=user.uid, service=service, node=user.node)
     auth = HawkAuth(token, secret)
+    if settings and settings.dryrun:
+        return
     resp = requests.delete(endpoint, auth=auth, timeout=timeout)
     if resp.status_code >= 400 and resp.status_code != 404:
         resp.raise_for_status()
@@ -125,7 +172,7 @@ def main(args=None):
     This function parses command-line arguments and passes them on
     to the purge_old_records() function.
     """
-    usage = "usage: %prog [options] secret"
+    usage = "usage: %prog [options] config_file"
     parser = optparse.OptionParser(usage=usage)
     parser.add_option("", "--purge-interval", type="int", default=3600,
                       help="Interval to sleep between purging runs")
@@ -141,23 +188,31 @@ def main(args=None):
                       help="Timeout for service deletion requests")
     parser.add_option("", "--oneshot", action="store_true",
                       help="Do a single purge run and then exit")
+    parser.add_option("", "--max-records", type="int", default=0,
+                      help="Max records to delete")
+    parser.add_option("", "--dryrun", action="store_true",
+                      help="Don't do destructive things")
+    parser.add_option("", "--force", action="store_true",
+                      help="force record to be deleted from TS db,"
+                      " even if node is down")
     parser.add_option("-v", "--verbose", action="count", dest="verbosity",
                       help="Control verbosity of log messages")
 
     opts, args = parser.parse_args(args)
-    if len(args) != 2:
+    if len(args) != 1:
         parser.print_usage()
         return 1
 
-    secret = args[1]
+    tokenserver.scripts.configure_script_logging(opts)
 
-    util.configure_script_logging(opts)
+    config_file = os.path.abspath(args[0])
 
-    purge_old_records(secret,
+    purge_old_records(config_file,
                       grace_period=opts.grace_period,
                       max_per_loop=opts.max_per_loop,
                       max_offset=opts.max_offset,
-                      request_timeout=opts.request_timeout)
+                      request_timeout=opts.request_timeout,
+                      settings=opts)
     if not opts.oneshot:
         while True:
             # Randomize sleep interval +/- thirty percent to desynchronize
@@ -166,12 +221,14 @@ def main(args=None):
             sleep_time += random.randint(-0.3 * sleep_time, 0.3 * sleep_time)
             logger.debug("Sleeping for %d seconds", sleep_time)
             time.sleep(sleep_time)
-            purge_old_records(grace_period=opts.grace_period,
+            purge_old_records(config_file,
+                              grace_period=opts.grace_period,
                               max_per_loop=opts.max_per_loop,
                               max_offset=opts.max_offset,
-                              request_timeout=opts.request_timeout)
+                              request_timeout=opts.request_timeout,
+                              settings=opts)
     return 0
 
 
 if __name__ == "__main__":
-    util.run_script(main)
+    tokenserver.scripts.run_script(main)
