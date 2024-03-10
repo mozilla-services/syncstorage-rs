4da5bc0a2c2c6e3bd3829622c44e4cf3b771ef13
Eragon <eragon@eragon.re>
2024-03-10 22:27:48 +0100

    feat: Add sqlite as a storage backend
    First throw at adding sqlite as storage backend for the syncserver and
    the tokenserver.
    
    There is probably some duplicated code between:
    - syncstorage-mysql and syncstorage-sqlite
    - tokenserver-db-mysql and tokenserver-db-sqlite
    
    tokenserver-db-sqlite probably contains Mysql-specific SQL that might
    break Sqlite when ran.
    
    Squashed commit of the following:
    
    commit 104719797fd5562a3e14bb7d6a265e8479715a29
    Author: Eragon <eragon@eragon.re>
    Date:   Wed Mar 6 00:24:07 2024 +0100
    
        fix: Fix default for tokenserver-db
    
    commit 5e2a745fb5235bb7f3197c705b6eb0886afd81df
    Author: Eragon <eragon@eragon.re>
    Date:   Sun Feb 11 22:00:46 2024 +0100
    
        tokenserver-db defaults to use mysql as db backend
    
    commit a5877878a9fb8b540b86ae13ec19afa5252f2635
    Author: Eragon <eragon@eragon.re>
    Date:   Sun Feb 11 21:47:55 2024 +0100
    
        Run cargo fmt
    
    commit c8c1458f535e607645b0a3780b3601a07818c6ef
    Author: Eragon <eragon@eragon.re>
    Date:   Sun Feb 11 20:32:19 2024 +0100
    
        Better logging of migrations
    
    commit e2b856333dd879da4e8a39967e87897098d640ee
    Author: Eragon <eragon@eragon.re>
    Date:   Tue Jan 30 00:36:11 2024 +0100
    
        wip: At least it runs now
    
    commit bd24d7cb8b51d55d65db266255f94f4315592e60
    Author: Eragon <eragon@eragon.re>
    Date:   Mon Jan 22 11:20:38 2024 +0100
    
        lll
    
    commit 07ba38f8789c64d060d7ea723757b1b14f447b9b
    Author: Eragon <eragon@eragon.re>
    Date:   Fri Jan 19 14:35:20 2024 +0100
    
        wip: First throw at adding sqlite as a storage backend

--- syncserver-db-common/src/error.rs
+++ syncserver-db-common/src/error.rs
@@ -8,14 +8,14 @@ use thiserror::Error;
 /// Error specific to any MySQL database backend. These errors are not related to the syncstorage
 /// or tokenserver application logic; rather, they are lower-level errors arising from diesel.
 #[derive(Debug)]
-pub struct MysqlError {
-    kind: MysqlErrorKind,
+pub struct SqlError {
+    kind: SqlErrorKind,
     pub status: StatusCode,
     pub backtrace: Backtrace,
 }
 
 #[derive(Debug, Error)]
-enum MysqlErrorKind {
+enum SqlErrorKind {
     #[error("A database error occurred: {}", _0)]
     DieselQuery(#[from] diesel::result::Error),
 
@@ -29,8 +29,8 @@ enum MysqlErrorKind {
     Migration(diesel_migrations::RunMigrationsError),
 }
 
-impl From<MysqlErrorKind> for MysqlError {
-    fn from(kind: MysqlErrorKind) -> Self {
+impl From<SqlErrorKind> for SqlError {
+    fn from(kind: SqlErrorKind) -> Self {
         Self {
             kind,
             status: StatusCode::INTERNAL_SERVER_ERROR,
@@ -39,11 +39,11 @@ impl From<MysqlErrorKind> for MysqlError {
     }
 }
 
-impl ReportableError for MysqlError {
+impl ReportableError for SqlError {
     fn is_sentry_event(&self) -> bool {
         #[allow(clippy::match_like_matches_macro)]
         match &self.kind {
-            MysqlErrorKind::Pool(_) => false,
+            SqlErrorKind::Pool(_) => false,
             _ => true,
         }
     }
@@ -51,10 +51,10 @@ impl ReportableError for MysqlError {
     fn metric_label(&self) -> Option<String> {
         Some(
             match self.kind {
-                MysqlErrorKind::DieselQuery(_) => "diesel_query",
-                MysqlErrorKind::DieselConnection(_) => "diesel_connection",
-                MysqlErrorKind::Pool(_) => "pool",
-                MysqlErrorKind::Migration(_) => "migration",
+                SqlErrorKind::DieselQuery(_) => "diesel_query",
+                SqlErrorKind::DieselConnection(_) => "diesel_connection",
+                SqlErrorKind::Pool(_) => "pool",
+                SqlErrorKind::Migration(_) => "migration",
             }
             .to_string(),
         )
@@ -65,21 +65,17 @@ impl ReportableError for MysqlError {
     }
 }
 
-impl_fmt_display!(MysqlError, MysqlErrorKind);
+impl_fmt_display!(SqlError, SqlErrorKind);
 
-from_error!(
-    diesel::result::Error,
-    MysqlError,
-    MysqlErrorKind::DieselQuery
-);
+from_error!(diesel::result::Error, SqlError, SqlErrorKind::DieselQuery);
 from_error!(
     diesel::result::ConnectionError,
-    MysqlError,
-    MysqlErrorKind::DieselConnection
+    SqlError,
+    SqlErrorKind::DieselConnection
 );
-from_error!(diesel::r2d2::PoolError, MysqlError, MysqlErrorKind::Pool);
+from_error!(diesel::r2d2::PoolError, SqlError, SqlErrorKind::Pool);
 from_error!(
     diesel_migrations::RunMigrationsError,
-    MysqlError,
-    MysqlErrorKind::Migration
+    SqlError,
+    SqlErrorKind::Migration
 );
--- syncstorage-sqlite/src/error.rs
+++ syncstorage-sqlite/src/error.rs
@@ -49,7 +49,7 @@ enum DbErrorKind {
     Common(SyncstorageDbError),
 
     #[error("{}", _0)]
-    Mysql(SqlError),
+    Sqlite(SqlError),
 }
 
 impl From<DbErrorKind> for DbError {
@@ -95,35 +95,35 @@ impl ReportableError for DbError {
     fn reportable_source(&self) -> Option<&(dyn ReportableError + 'static)> {
         Some(match &self.kind {
             DbErrorKind::Common(e) => e,
-            DbErrorKind::Mysql(e) => e,
+            DbErrorKind::Sqlite(e) => e,
         })
     }
 
     fn is_sentry_event(&self) -> bool {
         match &self.kind {
             DbErrorKind::Common(e) => e.is_sentry_event(),
-            DbErrorKind::Mysql(e) => e.is_sentry_event(),
+            DbErrorKind::Sqlite(e) => e.is_sentry_event(),
         }
     }
 
     fn metric_label(&self) -> Option<String> {
         match &self.kind {
             DbErrorKind::Common(e) => e.metric_label(),
-            DbErrorKind::Mysql(e) => e.metric_label(),
+            DbErrorKind::Sqlite(e) => e.metric_label(),
         }
     }
 
     fn backtrace(&self) -> Option<&Backtrace> {
         match &self.kind {
             DbErrorKind::Common(e) => e.backtrace(),
-            DbErrorKind::Mysql(e) => e.backtrace(),
+            DbErrorKind::Sqlite(e) => e.backtrace(),
         }
     }
 
     fn tags(&self) -> Vec<(&str, String)> {
         match &self.kind {
             DbErrorKind::Common(e) => e.tags(),
-            DbErrorKind::Mysql(e) => e.tags(),
+            DbErrorKind::Sqlite(e) => e.tags(),
         }
     }
 }
@@ -140,24 +140,24 @@ from_error!(SyncstorageDbError, DbError, DbErrorKind::Common);
 from_error!(
     diesel::result::Error,
     DbError,
-    |error: diesel::result::Error| DbError::from(DbErrorKind::Mysql(SqlError::from(error)))
+    |error: diesel::result::Error| DbError::from(DbErrorKind::Sqlite(SqlError::from(error)))
 );
 from_error!(
     diesel::result::ConnectionError,
     DbError,
-    |error: diesel::result::ConnectionError| DbError::from(DbErrorKind::Mysql(SqlError::from(
+    |error: diesel::result::ConnectionError| DbError::from(DbErrorKind::Sqlite(SqlError::from(
         error
     )))
 );
 from_error!(
     diesel::r2d2::PoolError,
     DbError,
-    |error: diesel::r2d2::PoolError| DbError::from(DbErrorKind::Mysql(SqlError::from(error)))
+    |error: diesel::r2d2::PoolError| DbError::from(DbErrorKind::Sqlite(SqlError::from(error)))
 );
 from_error!(
     diesel_migrations::RunMigrationsError,
     DbError,
-    |error: diesel_migrations::RunMigrationsError| DbError::from(DbErrorKind::Mysql(
+    |error: diesel_migrations::RunMigrationsError| DbError::from(DbErrorKind::Sqlite(
         SqlError::from(error)
     ))
 );
