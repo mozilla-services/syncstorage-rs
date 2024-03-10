fa03d087c3c3c3da7396443afc3efc87c85e0c61
Eragon <eragon@eragon.re>
2024-03-10 22:27:48 +0100

    feat: Add sqlite as a storage backend
    First throw at adding sqlite as storage backend for the syncserver and
    the tokenserver.
    
    There is probably some duplicated code between:
    - syncstorage-mysql and syncstorage-sqlite
    - tokenserver-db-mysql and tokenserver-db-sqlite
    
    tokenserver-db-sqlite probably contains Mysql-specific SQL that might
    break Sqlite when ran.
    
    Squashed commit of the following:
    
    commit 104719797fd5562a3e14bb7d6a265e8479715a29
    Author: Eragon <eragon@eragon.re>
    Date:   Wed Mar 6 00:24:07 2024 +0100
    
        fix: Fix default for tokenserver-db
    
    commit 5e2a745fb5235bb7f3197c705b6eb0886afd81df
    Author: Eragon <eragon@eragon.re>
    Date:   Sun Feb 11 22:00:46 2024 +0100
    
        tokenserver-db defaults to use mysql as db backend
    
    commit a5877878a9fb8b540b86ae13ec19afa5252f2635
    Author: Eragon <eragon@eragon.re>
    Date:   Sun Feb 11 21:47:55 2024 +0100
    
        Run cargo fmt
    
    commit c8c1458f535e607645b0a3780b3601a07818c6ef
    Author: Eragon <eragon@eragon.re>
    Date:   Sun Feb 11 20:32:19 2024 +0100
    
        Better logging of migrations
    
    commit e2b856333dd879da4e8a39967e87897098d640ee
    Author: Eragon <eragon@eragon.re>
    Date:   Tue Jan 30 00:36:11 2024 +0100
    
        wip: At least it runs now
    
    commit bd24d7cb8b51d55d65db266255f94f4315592e60
    Author: Eragon <eragon@eragon.re>
    Date:   Mon Jan 22 11:20:38 2024 +0100
    
        lll
    
    commit 07ba38f8789c64d060d7ea723757b1b14f447b9b
    Author: Eragon <eragon@eragon.re>
    Date:   Fri Jan 19 14:35:20 2024 +0100
    
        wip: First throw at adding sqlite as a storage backend

---
+++ syncstorage-sqlite/src/error.rs
@@ -0,0 +1,163 @@
+use std::fmt;
+
+use backtrace::Backtrace;
+use http::StatusCode;
+use syncserver_common::{from_error, impl_fmt_display, InternalError, ReportableError};
+use syncserver_db_common::error::SqlError;
+use syncstorage_db_common::error::{DbErrorIntrospect, SyncstorageDbError};
+use thiserror::Error;
+
+/// An error type that represents any MySQL-related errors that may occur while processing a
+/// syncstorage request. These errors may be application-specific or lower-level errors that arise
+/// from the database backend.
+#[derive(Debug)]
+pub struct DbError {
+    kind: DbErrorKind,
+    pub status: StatusCode,
+    pub backtrace: Box<Backtrace>,
+}
+
+impl DbError {
+    pub fn batch_not_found() -> Self {
+        DbErrorKind::Common(SyncstorageDbError::batch_not_found()).into()
+    }
+
+    pub fn bso_not_found() -> Self {
+        DbErrorKind::Common(SyncstorageDbError::bso_not_found()).into()
+    }
+
+    pub fn collection_not_found() -> Self {
+        DbErrorKind::Common(SyncstorageDbError::collection_not_found()).into()
+    }
+
+    pub fn conflict() -> Self {
+        DbErrorKind::Common(SyncstorageDbError::conflict()).into()
+    }
+
+    pub fn internal(msg: String) -> Self {
+        DbErrorKind::Common(SyncstorageDbError::internal(msg)).into()
+    }
+
+    pub fn quota() -> Self {
+        DbErrorKind::Common(SyncstorageDbError::quota()).into()
+    }
+}
+
+#[derive(Debug, Error)]
+enum DbErrorKind {
+    #[error("{}", _0)]
+    Common(SyncstorageDbError),
+
+    #[error("{}", _0)]
+    Sqlite(SqlError),
+}
+
+impl From<DbErrorKind> for DbError {
+    fn from(kind: DbErrorKind) -> Self {
+        match &kind {
+            DbErrorKind::Common(dbe) => Self {
+                status: dbe.status,
+                backtrace: Box::new(dbe.backtrace.clone()),
+                kind,
+            },
+            _ => Self {
+                kind,
+                status: StatusCode::INTERNAL_SERVER_ERROR,
+                backtrace: Box::new(Backtrace::new()),
+            },
+        }
+    }
+}
+
+impl DbErrorIntrospect for DbError {
+    fn is_batch_not_found(&self) -> bool {
+        matches!(&self.kind, DbErrorKind::Common(e) if e.is_batch_not_found())
+    }
+
+    fn is_bso_not_found(&self) -> bool {
+        matches!(&self.kind, DbErrorKind::Common(e) if e.is_bso_not_found())
+    }
+
+    fn is_collection_not_found(&self) -> bool {
+        matches!(&self.kind, DbErrorKind::Common(e) if e.is_collection_not_found())
+    }
+
+    fn is_conflict(&self) -> bool {
+        matches!(&self.kind, DbErrorKind::Common(e) if e.is_conflict())
+    }
+
+    fn is_quota(&self) -> bool {
+        matches!(&self.kind, DbErrorKind::Common(e) if e.is_quota())
+    }
+}
+
+impl ReportableError for DbError {
+    fn reportable_source(&self) -> Option<&(dyn ReportableError + 'static)> {
+        Some(match &self.kind {
+            DbErrorKind::Common(e) => e,
+            DbErrorKind::Sqlite(e) => e,
+        })
+    }
+
+    fn is_sentry_event(&self) -> bool {
+        match &self.kind {
+            DbErrorKind::Common(e) => e.is_sentry_event(),
+            DbErrorKind::Sqlite(e) => e.is_sentry_event(),
+        }
+    }
+
+    fn metric_label(&self) -> Option<String> {
+        match &self.kind {
+            DbErrorKind::Common(e) => e.metric_label(),
+            DbErrorKind::Sqlite(e) => e.metric_label(),
+        }
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        match &self.kind {
+            DbErrorKind::Common(e) => e.backtrace(),
+            DbErrorKind::Sqlite(e) => e.backtrace(),
+        }
+    }
+
+    fn tags(&self) -> Vec<(&str, String)> {
+        match &self.kind {
+            DbErrorKind::Common(e) => e.tags(),
+            DbErrorKind::Sqlite(e) => e.tags(),
+        }
+    }
+}
+
+impl InternalError for DbError {
+    fn internal_error(message: String) -> Self {
+        DbErrorKind::Common(SyncstorageDbError::internal(message)).into()
+    }
+}
+
+impl_fmt_display!(DbError, DbErrorKind);
+
+from_error!(SyncstorageDbError, DbError, DbErrorKind::Common);
+from_error!(
+    diesel::result::Error,
+    DbError,
+    |error: diesel::result::Error| DbError::from(DbErrorKind::Sqlite(SqlError::from(error)))
+);
+from_error!(
+    diesel::result::ConnectionError,
+    DbError,
+    |error: diesel::result::ConnectionError| DbError::from(DbErrorKind::Sqlite(SqlError::from(
+        error
+    )))
+);
+from_error!(
+    diesel::r2d2::PoolError,
+    DbError,
+    |error: diesel::r2d2::PoolError| DbError::from(DbErrorKind::Sqlite(SqlError::from(error)))
+);
+from_error!(
+    diesel_migrations::RunMigrationsError,
+    DbError,
+    |error: diesel_migrations::RunMigrationsError| DbError::from(DbErrorKind::Sqlite(
+        SqlError::from(error)
+    ))
+);
--- tokenserver-db/src/lib.rs
+++ tokenserver-db/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate diesel;
-#[macro_use]
 extern crate diesel_migrations;
 #[macro_use]
 extern crate slog_scope;
 
-mod error;
+use diesel::r2d2::{ConnectionManager, PooledConnection};
+#[cfg(feature = "mysql")]
+use diesel::MysqlConnection;
+#[cfg(feature = "sqlite")]
+use diesel::SqliteConnection;
+
 pub mod mock;
 mod models;
 pub mod params;
@@ -13,3 +17,9 @@ pub mod results;
 
 pub use models::{Db, TokenserverDb};
 pub use pool::{DbPool, TokenserverPool};
+
+#[cfg(feature = "mysql")]
+type Conn = MysqlConnection;
+#[cfg(feature = "sqlite")]
+type Conn = SqliteConnection;
+type PooledConn = PooledConnection<ConnectionManager<Conn>>;
