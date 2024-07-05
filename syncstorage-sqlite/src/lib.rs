3e6c01c008e5de1aa1a45caa6d1d2a8efda05e59
Eragon <eragon@eragon.re>
2024-07-05 18:53:01 +0200

    refactor: Add shared crate for common code of SQL backends

--- syncstorage-sqlite/src/lib.rs
+++ syncstorage-sqlite/src/lib.rs
@@ -8,15 +8,14 @@ extern crate slog_scope;
 #[macro_use]
 mod batch;
 mod diesel_ext;
-mod error;
 mod models;
 mod pool;
 mod schema;
 #[cfg(test)]
 mod test;
 
-pub use error::DbError;
+pub use syncstorage_sql_db_common::error::DbError;
 pub use models::SqliteDb;
 pub use pool::SqliteDbPool;
 
-pub(crate) type DbResult<T> = Result<T, error::DbError>;
+pub(crate) type DbResult<T> = Result<T, DbError>;
