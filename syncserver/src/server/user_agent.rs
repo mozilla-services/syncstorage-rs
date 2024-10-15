001b55f8550461771e17c71a78e13e02edb29b40
Taddes <tkorris@mozilla.com>
2024-10-15 18:21:58 -0400

    updated parsing and device info function to support platform

--- syncserver/src/server/user_agent.rs
+++ syncserver/src/server/user_agent.rs
@@ -15,10 +15,25 @@ const VALID_UA_BROWSER: &[&str] = &["Chrome", "Firefox", "Safari", "Opera"];
 const VALID_UA_OS: &[&str] = &["Firefox OS", "Linux", "Mac OSX"];
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Platform {
+    FirefoxDesktop,
+    Fenix,
+    FirefoxIOS,
+    Other,
+}
+
+impl fmt::Display for Platform {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = format!("{:?}", self).to_lowercase();
+        write!(fmt, "{}", name)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum DeviceFamily {
     Desktop,
-    IOS,
-    Android,
+    Mobile,
+    Tablet,
     Other,
 }
 
@@ -36,7 +51,6 @@ pub enum OsFamily {
     Linux,
     IOS,
     Android,
-    ChromeOs,
     Other,
 }
 
@@ -49,6 +63,7 @@ impl fmt::Display for OsFamily {
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct DeviceInfo {
+    pub platform: Platform,
     pub device_family: DeviceFamily,
     pub os_family: OsFamily,
     pub firefox_version: u32,
@@ -66,20 +81,19 @@ impl DeviceInfo {
 
     /// Determine if the device is a mobile phone based on either the form factor or OS.
     pub fn is_mobile(&self) -> bool {
-        matches!(
-            &self.device_family,
-            DeviceFamily::IOS | DeviceFamily::Android
-        ) || matches!(&self.os_family, OsFamily::Android | OsFamily::IOS)
+        matches!(&self.device_family, DeviceFamily::Mobile)
+            && matches!(&self.os_family, OsFamily::Android | OsFamily::IOS)
     }
 
     /// Determine if the device is iOS based on either the form factor or OS.
     pub fn is_ios(&self) -> bool {
-        matches!(&self.device_family, DeviceFamily::IOS) && matches!(&self.os_family, OsFamily::IOS)
+        matches!(&self.device_family, DeviceFamily::Mobile)
+            && matches!(&self.os_family, OsFamily::IOS)
     }
 
     /// Determine if the device is an android (Fenix) device based on either the form factor or OS.
     pub fn is_fenix(&self) -> bool {
-        matches!(&self.device_family, DeviceFamily::Android)
+        matches!(&self.device_family, DeviceFamily::Mobile)
             && matches!(&self.os_family, OsFamily::Android)
     }
 }
@@ -96,6 +110,8 @@ pub fn get_device_info(user_agent: &str) -> DeviceInfo {
             version: "",
             vendor: "",
         });
+    let firefox_version =
+        u32::from_str(w_result.version.split(".").collect::<Vec<&str>>()[0]).unwrap_or_default();
 
     // NOTE: Firefox on iPads report back the Safari "desktop" UA
     // (e.g. `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15
@@ -108,25 +124,39 @@ pub fn get_device_info(user_agent: &str) -> DeviceInfo {
         w_result.os = "ipad";
     }
 
-    let firefox_version =
-        u32::from_str(w_result.version.split(".").collect::<Vec<&str>>()[0]).unwrap_or_default();
     let os = w_result.os.to_lowercase();
     let os_family = match os.as_str() {
         _ if os.starts_with("windows") => OsFamily::Windows,
         "mac osx" => OsFamily::MacOs,
         "linux" => OsFamily::Linux,
-        "iphone" => OsFamily::IOS,
+        "iphone" | "ipad" => OsFamily::IOS,
         "android" => OsFamily::Android,
-        "chromeos" => OsFamily::ChromeOs,
         _ => OsFamily::Other,
     };
+
     let device_family = match w_result.category {
         "pc" => DeviceFamily::Desktop,
-        "smartphone" if os.as_str() == "ipad" => DeviceFamily::IOS,
-        "smartphone" if os.as_str() == "android" => DeviceFamily::Android,
+        "smartphone" if os.as_str() == "ipad" => DeviceFamily::Tablet,
+        "smartphone" => DeviceFamily::Mobile,
         _ => DeviceFamily::Other,
     };
+
+    let platform = match device_family {
+        DeviceFamily::Desktop => Platform::FirefoxDesktop,
+        DeviceFamily::Mobile => match os_family {
+            OsFamily::IOS => Platform::FirefoxIOS,
+            OsFamily::Android => Platform::Fenix,
+            _ => Platform::Other,
+        },
+        DeviceFamily::Tablet => match os_family {
+            OsFamily::IOS => Platform::FirefoxIOS,
+            _ => Platform::Other,
+        },
+        DeviceFamily::Other => Platform::Other,
+    };
+
     DeviceInfo {
+        platform,
         device_family,
         os_family,
         firefox_version,
@@ -163,7 +193,9 @@ pub fn parse_user_agent(agent: &str) -> (WootheeResult<'_>, &str, &str) {
 
 #[cfg(test)]
 mod tests {
-    use super::parse_user_agent;
+    use crate::server::user_agent::{DeviceFamily, OsFamily, Platform};
+
+    use super::{get_device_info, parse_user_agent};
 
     #[test]
     fn test_linux() {
@@ -202,5 +234,15 @@ mod tests {
         assert_eq!(ua_result.os, "BlackBerry");
         assert_eq!(metrics_browser, "Other");
         assert_eq!(ua_result.name, "UNKNOWN");
+    }
+
+    #[test]
+    fn test_desktop() {
+        let desktop_user_agent = r#"Firefox/130.0.1 (Windows NT 10.0; Win64; x64) FxSync/1.132.0.20240913135723.desktop"#;
+        let device_info = get_device_info(desktop_user_agent);
+        assert_eq!(device_info.platform, Platform::FirefoxDesktop);
+        assert_eq!(device_info.device_family, DeviceFamily::Desktop);
+        assert_eq!(device_info.os_family, OsFamily::Windows);
+        assert_eq!(device_info.firefox_version, 130);
     }
 }
