3f5345f1d461574d29238805c29ce6f4edb60107
Eragon <eragon@eragon.re>
2024-11-20 23:23:03 +0100

    feat: Put MySQL and Sqlite test connexions behind flag

--- syncserver-db-common/src/test.rs
+++ syncserver-db-common/src/test.rs
@@ -1,19 +1,24 @@
 use diesel::{
-    mysql::MysqlConnection,
     r2d2::{CustomizeConnection, Error as PoolError},
-    sqlite::SqliteConnection,
     Connection,
 };
+
+#[cfg(feature = "mysql")]
+use diesel::mysql::MysqlConnection;
+#[cfg(feature = "sqlite")]
+use diesel::sqlite::SqliteConnection;
 
 #[derive(Debug)]
 pub struct TestTransactionCustomizer;
 
+#[cfg(feature = "mysql")]
 impl CustomizeConnection<MysqlConnection, PoolError> for TestTransactionCustomizer {
     fn on_acquire(&self, conn: &mut MysqlConnection) -> Result<(), PoolError> {
         conn.begin_test_transaction().map_err(PoolError::QueryError)
     }
 }
 
+#[cfg(feature = "sqlite")]
 impl CustomizeConnection<SqliteConnection, PoolError> for TestTransactionCustomizer {
     fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), PoolError> {
         conn.begin_test_transaction().map_err(PoolError::QueryError)
