a81275abd43e5bebdf14c206b5ba437a92aa05ef
Taddes <tkorris@mozilla.com>
2024-12-05 11:55:04 -0500

    add mysql noop impl

--- syncstorage-mysql/src/pool.rs
+++ syncstorage-mysql/src/pool.rs
@@ -99,6 +99,13 @@ impl MysqlDbPool {
         })
     }
 
+    /// Spawn a task to periodically evict idle connections. Calls wrapper sweeper fn
+    ///  to use pool.retain, retaining objects only if they are shorter in duration than
+    ///  defined max_idle. Noop for mysql impl.
+    pub fn spawn_sweeper(&self, _interval: Duration) {
+        sweeper()
+    }
+
     pub fn get_sync(&self) -> DbResult<MysqlDb> {
         Ok(MysqlDb::new(
             self.pool.get()?,
@@ -109,6 +116,13 @@ impl MysqlDbPool {
         ))
     }
 }
+
+/// Sweeper to retain only the objects specified within the closure.
+/// In this context, if a Spanner connection is unutilized, we want it
+/// to release the given connections.
+/// See: https://docs.rs/deadpool/latest/deadpool/managed/struct.Pool.html#method.retain
+/// Noop for mysql impl
+fn sweeper() {}
 
 #[async_trait]
 impl DbPool for MysqlDbPool {
