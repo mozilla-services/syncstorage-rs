b7fec948834e11a5f0c95145d1327900e2c6658f
Philip Jenvey <pjenvey@underboss.org>
2025-07-04 23:43:20 -0700

    chore: require minimum mysql 8
    BREAKING CHANGE: now depending on >= mysql 8 features
    
    Closes STOR-160

--- syncstorage-mysql/src/diesel_ext.rs
+++ syncstorage-mysql/src/diesel_ext.rs
@@ -1,42 +1,10 @@
 use diesel::{
     backend::Backend,
     insertable::CanInsertInSingleQuery,
-    mysql::Mysql,
     query_builder::{AstPass, InsertStatement, QueryFragment, QueryId},
-    query_dsl::methods::LockingDsl,
     result::QueryResult,
     Expression, RunQueryDsl, Table,
 };
-
-/// Emit MySQL <= 5.7's `LOCK IN SHARE MODE`
-///
-/// MySQL 8 supports `FOR SHARE` as an alias (which diesel natively supports)
-pub trait LockInShareModeDsl {
-    type Output;
-
-    fn lock_in_share_mode(self) -> Self::Output;
-}
-
-impl<T> LockInShareModeDsl for T
-where
-    T: LockingDsl<LockInShareMode>,
-{
-    type Output = <T as LockingDsl<LockInShareMode>>::Output;
-
-    fn lock_in_share_mode(self) -> Self::Output {
-        self.with_lock(LockInShareMode)
-    }
-}
-
-#[derive(Debug, Clone, Copy, QueryId)]
-pub struct LockInShareMode;
-
-impl QueryFragment<Mysql> for LockInShareMode {
-    fn walk_ast(&self, mut out: AstPass<'_, Mysql>) -> QueryResult<()> {
-        out.push_sql(" LOCK IN SHARE MODE");
-        Ok(())
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct OnDuplicateKeyUpdate<T, U, Op, Ret, X>(Box<InsertStatement<T, U, Op, Ret>>, X);
