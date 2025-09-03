56f2fc34b5bbbe8f0c0d65fc0db06e26cb4fee0f
Taddes <tkorris@mozilla.com>
2025-09-03 13:04:00 -0400

    Config refactor and update

--- syncserver-settings/src/lib.rs
+++ syncserver-settings/src/lib.rs
@@ -55,11 +55,11 @@ pub struct Settings {
 impl Settings {
     /// Load the settings from the config file if supplied, then the environment.
     pub fn with_env_and_config_file(filename: Option<&str>) -> Result<Self, ConfigError> {
-        let mut s = Config::default();
+        let mut builder = Config::builder();
 
         // Merge the config file if supplied
         if let Some(config_filename) = filename {
-            s.merge(File::with_name(config_filename))?;
+            builder = builder.add_source(File::with_name(config_filename));
         }
 
         // Merge the environment overrides
@@ -68,7 +68,9 @@ impl Settings {
         // Environment ever change their policy about case insensitivity.
         // This will accept environment variables specified as
         // `SYNC_FOO__BAR_VALUE="gorp"` as `foo.bar_value = "gorp"`
-        s.merge(Environment::with_prefix(&PREFIX.to_uppercase()).separator("__"))?;
+        builder =
+            builder.add_source(Environment::with_prefix(&PREFIX.to_uppercase()).separator("__"));
+        let settings: Config = builder.build()?;
 
         let mut s = s.try_into::<Self>().map_err(|e| match e {
             ConfigError::Message(v) => {
