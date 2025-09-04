cf1be4304b163bc2c75f19dc6eacf0b5f454b74f
Philip Jenvey <pjenvey@underboss.org>
2025-09-03 22:58:54 -0700

    .

--- syncserver-db-common/src/test.rs
+++ syncserver-db-common/src/test.rs
@@ -3,7 +3,7 @@ use diesel::{
     r2d2::{CustomizeConnection, Error as PoolError},
     Connection,
 };
-use diesel_async::{AsyncConnection, AsyncMysqlConnection};
+use diesel_async::AsyncConnection;
 
 #[derive(Debug)]
 pub struct TestTransactionCustomizer;
@@ -14,9 +14,12 @@ impl CustomizeConnection<MysqlConnection, PoolError> for TestTransactionCustomiz
     }
 }
 
-pub async fn test_transaction_hook(
-    conn: &mut AsyncMysqlConnection,
-) -> deadpool::managed::HookResult<diesel_async::pooled_connection::PoolError> {
+pub async fn test_transaction_hook<T>(
+    conn: &mut T,
+) -> deadpool::managed::HookResult<diesel_async::pooled_connection::PoolError>
+where
+    T: AsyncConnection,
+{
     conn.begin_test_transaction().await.map_err(|e| {
         deadpool::managed::HookError::Backend(
             diesel_async::pooled_connection::PoolError::QueryError(e),
