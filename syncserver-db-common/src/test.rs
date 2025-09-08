f2698a4251cd719413b2071379f6866b50d4981a
Philip Jenvey <pjenvey@underboss.org>
2025-09-08 13:08:29 -0700

    feat: switch tokenserver to diesel-async (#1790)
    Closes STOR-326

--- syncserver-db-common/src/test.rs
+++ syncserver-db-common/src/test.rs
@@ -1,14 +1,22 @@
-use diesel::{
-    mysql::MysqlConnection,
-    r2d2::{CustomizeConnection, Error as PoolError},
-    Connection,
-};
+use deadpool::managed::{HookError, HookResult};
+use diesel::{mysql::MysqlConnection, r2d2::CustomizeConnection, Connection};
+use diesel_async::{pooled_connection::PoolError, AsyncConnection};
 
 #[derive(Debug)]
 pub struct TestTransactionCustomizer;
 
-impl CustomizeConnection<MysqlConnection, PoolError> for TestTransactionCustomizer {
-    fn on_acquire(&self, conn: &mut MysqlConnection) -> Result<(), PoolError> {
-        conn.begin_test_transaction().map_err(PoolError::QueryError)
+impl CustomizeConnection<MysqlConnection, diesel::r2d2::Error> for TestTransactionCustomizer {
+    fn on_acquire(&self, conn: &mut MysqlConnection) -> Result<(), diesel::r2d2::Error> {
+        conn.begin_test_transaction()
+            .map_err(diesel::r2d2::Error::QueryError)
     }
+}
+
+pub async fn test_transaction_hook<T>(conn: &mut T) -> HookResult<PoolError>
+where
+    T: AsyncConnection,
+{
+    conn.begin_test_transaction()
+        .await
+        .map_err(|e| HookError::Backend(PoolError::QueryError(e)))
 }
