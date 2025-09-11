9d799a45d41d1f09589cd01ac4df38decb7a9548
Philip Jenvey <pjenvey@underboss.org>
2025-09-11 00:02:00 -0700

    refactor: move tokenserver's Db to async-trait (#1799)
    Closes STOR-142

--- tokenserver-db/src/lib.rs
+++ tokenserver-db/src/lib.rs
@@ -13,15 +13,3 @@ pub mod results;
 
 pub use models::{Db, TokenserverDb};
 pub use pool::{DbPool, TokenserverPool};
-
-#[macro_export]
-macro_rules! async_db_method {
-    ($name:ident, $async_name:path, $type:ident) => {
-        async_db_method!($name, $async_name, $type, results::$type);
-    };
-    ($name:ident, $async_name:path, $type:ident, $result:ty) => {
-        fn $name(&mut self, params: params::$type) -> DbFuture<'_, $result, DbError> {
-            Box::pin($async_name(self, params))
-        }
-    };
-}
