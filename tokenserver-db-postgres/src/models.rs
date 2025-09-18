fba3d5fa2032c0ecc6c94e0daf39549569dfa757
Taddes <tkorris@mozilla.com>
2025-09-18 12:32:37 -0400

    services methods

--- tokenserver-db-postgres/src/models.rs
+++ tokenserver-db-postgres/src/models.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use super::pool::Conn;
 use async_trait::async_trait;
 use syncserver_common::Metrics;
-use tokenserver_db_common::Db;
+use tokenserver_db_common::{error::DbError, params, results, Db};
 
 #[allow(dead_code)]
 pub struct TokenserverPgDb {
@@ -33,4 +33,28 @@ impl TokenserverPgDb {
 }
 
 #[async_trait(?Send)]
-impl Db for TokenserverPgDb {}
+impl Db for TokenserverPgDb {
+    fn timeout(&self) -> Option<Duration> {
+        self.timeout
+    }
+
+    // Services Methods
+    async fn get_service_id(
+        &mut self,
+        params: params::GetServiceId,
+    ) -> Result<results::GetServiceId, DbError> {
+        TokenserverPgDb::get_service_id(self, params).await
+    }
+
+    #[cfg(debug_assertions)]
+    async fn post_service(
+        &mut self,
+        params: params::PostService,
+    ) -> Result<results::PostService, DbError> {
+        TokenserverPgDb::post_service(self, params).await
+    }
+
+    // Nodes Methods
+
+    // Users Methods
+}
