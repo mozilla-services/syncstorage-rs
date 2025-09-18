6a00df82b2f3406ddce3d99240f78f4ae5dcc52e
Philip Jenvey <pjenvey@underboss.org>
2025-09-17 18:50:05 -0700

    simplify w/ a metrics method

--- tokenserver-db/src/models.rs
+++ tokenserver-db/src/models.rs
@@ -331,15 +331,6 @@ impl TokenserverDb {
             .map_err(Into::into)
     }
 
-    async fn allocate_user(
-        &mut self,
-        params: params::AllocateUser,
-    ) -> DbResult<results::AllocateUser> {
-        let mut metrics = self.metrics.clone();
-        metrics.start_timer("storage.allocate_user", None);
-        tokenserver_db_common::allocate_user(self, params).await
-    }
-
     pub async fn get_service_id(
         &mut self,
         params: params::GetServiceId,
@@ -584,11 +575,8 @@ impl Db for TokenserverDb {
         self.timeout
     }
 
-    async fn allocate_user(
-        &mut self,
-        params: params::AllocateUser,
-    ) -> Result<results::AllocateUser, DbError> {
-        TokenserverDb::allocate_user(self, params).await
+    fn timeout(&self) -> Option<Duration> {
+        self.timeout
     }
 
     #[cfg(debug_assertions)]
