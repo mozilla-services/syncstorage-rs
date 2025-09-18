1eaa24ee27350a20441ebbbb69dba66444dcfd5a
Taddes <tkorris@mozilla.com>
2025-09-18 13:47:35 -0400

    relabel unassign_node to unassign_node_from_users for clarity

--- tokenserver-db/src/mock.rs
+++ tokenserver-db/src/mock.rs
@@ -142,7 +142,7 @@ impl Db for MockDb {
     }
 
     #[cfg(debug_assertions)]
-    async fn unassign_node(
+    async fn unassign_node_from_users(
         &mut self,
         _params: params::UnassignNode,
     ) -> Result<results::UnassignNode, DbError> {
