929ba5d208b976894ceaf3177ebec391997d5c44
Taddes <tkorris@mozilla.com>
2025-09-23 14:45:27 -0400

    table models for orm schemas  (#1811)
    table models for orm schemas

--- tokenserver-db-postgres/src/lib.rs
+++ tokenserver-db-postgres/src/lib.rs
@@ -3,7 +3,11 @@ extern crate diesel;
 extern crate diesel_migrations;
 
 mod models;
+mod orm_models;
 mod pool;
+mod schema;
 
 pub use models::TokenserverPgDb;
+pub use orm_models::{Node, Service, User};
 pub use pool::TokenserverPgPool;
+pub use tokenserver_db_common::{params, results, Db, DbPool};
