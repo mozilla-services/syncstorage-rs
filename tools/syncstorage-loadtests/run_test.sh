93bb497aa5063a5abd4d3558c2331aa94b590307
Taddes <tkorris@mozilla.com>
2026-02-13 13:18:20 -0500

    update repo dir

--- tools/syncstorage-loadtests/run_test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -xe
-# the Sync server URL. (Presumes a locally running server.)
-# *NOTE* this will use the first arg, but will default to `localhost:8000`
-HOST=${1:-http://localhost:8000}
-# The sync shared secret.
-SECRET=${2:-secret0}
-SERVER_URL=${HOST}#${SECRET} poetry run molotov --processes 2 --workers 149 --verbose
