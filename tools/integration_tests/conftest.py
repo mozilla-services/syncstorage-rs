a980d1c5087b97741fbdfc61442f2e17238ab12d
Barry Chen <bchen@mozilla.com>
2026-04-10 08:26:02 -0500

    Remove integration_tests/conftest.py

--- tools/integration_tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Pytest configuration and fixtures for integration tests."""
-
-import os
-import logging
-
-SYNC_SERVER_URL = os.environ.get("SYNC_SERVER_URL", "http://localhost:8000")
-
-logger = logging.getLogger("tokenserver.scripts.conftest")
